438f22ee84eeffc54ad074a82bb895c05aaa0e45
Muratozturk1 <murat.murat.ozturk.ozturk@gmail.com>
2025-06-05 17:49:50 +0300

    Contact kısmına bakıldı

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -201,9 +201,6 @@ const Home = () => {
                 <div className="container mx-auto px-4">
                     <h2 className="text-3xl font-bold text-center mb-12">Neden Beni Tercih Etmelisiniz?</h2>
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-16">
-
-
-
                     </div>
 
                     <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
